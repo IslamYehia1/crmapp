080bbd7638d42592018074264684343c187814df
Islam <islamyehia1999@gmail.com>
2020-08-27 09:37:31 +0200

    Created the initial sign up form

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+var signUpForm = document.getElementById("signUpForm") ; 
+var signUpFeilds = document.querySelectorAll("input")
+console.log(signUpFeilds)
+signUpForm.addEventListener("submit", validateSubmittedForm)
+
+// If any field is empty or have only spaces
+// show an error msg 
+function validateSubmittedForm(event){
+
+    signUpFeilds.forEach(function isItValid(field){
+        event.preventDefault() ; 
+        if(!(field.value.trim.length >0)){
+            field.classList.add("is-invalid");
+            field.insertAdjacentHTML("afterend" , 
+            `<div id="" class="invalid-feedback">
+            Please provide a valid ${field.placeholder}.
+          </div>`)
+        }
+    })
+
+
+}
